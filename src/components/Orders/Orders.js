f306b8d2beb7c6d8906e196299d90c36981b8024
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-05-04 00:20:07 +0600

    cart added

---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Order = () => {
+    const products = useLoaderData();
+    return (
+        <div>
+
+            <h2>This is Orders: {products.length}</h2>
+        </div>
+    );
+};
+
+export default Order;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,20 +3,11 @@ import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
+import { useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
-    const [products, setProducts] = useState([])
-
+    const products = useLoaderData();
     const [cart, setCart] = useState([]);
-    useEffect(() => {
-        console.log('products load before fetch')
-        fetch('products.json')
-            .then(res => res.json())
-            .then(data => {
-                setProducts(data);
-                // console.log('Products loaded');
-            })
-    }, []);
 
     useEffect(() => {
         console.log('local storage first line', products)
