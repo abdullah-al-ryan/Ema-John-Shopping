f854c486ab72c027f0b3d1de26b21cb49b48ced4
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-05-05 00:42:00 +0600

    delete icon added

--- src/components/Review Item/ReviewItem.js
+++ src/components/Review Item/ReviewItem.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './ReviewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 const ReviewItem = ({ product }) => {
-    const { name, price, quantity, img } = product;
+    const { name, price, quantity, shipping, img } = product;
     return (
         <div className='review-item'>
             <div>
@@ -10,11 +12,14 @@ const ReviewItem = ({ product }) => {
             <div className="review-details-container">
                 <div className="review-details">
                     <p>{name}</p>
-                    <p><small>Price: {price}</small></p>
+                    <p><small>Price: ${price}</small></p>
+                    <p><small>Shipping: ${shipping}</small></p>
                     <p><small>Quantity: {quantity}</small></p>
                 </div>
                 <div className="delete-container">
-                    <button>Delete</button>
+                    <button className='btn-delete'>
+                        <FontAwesomeIcon className='delete-icon' icon={faTrashAlt}></FontAwesomeIcon>
+                    </button>
                 </div>
             </div>
         </div>
