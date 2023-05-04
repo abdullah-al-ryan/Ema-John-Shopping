f2ef606f03f5ccb65d91de1c9470a4b29c39622a
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-05-05 00:19:35 +0600

    reviewitem added

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,21 +1,28 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
+import ReviewItem from '../Review Item/ReviewItem';
 
 const Order = () => {
     const { products, initialCart } = useLoaderData();  // { products: products, initialCart: initialCart}
     const [cart, setCart] = useState(initialCart);
+    
     return (
         <div>
             <div className="shop-container">
+                <div className="orders-container">
+                 {
+                       cart.map(product => <ReviewItem
+                            key={product.id}
+                            product={product}
+                        ></ReviewItem>)
+                }
+                </div>
+                <div className="cart-container">
+                    <Cart cart={cart}></Cart>
+                </div>
+            </div>
 
-            </div>
-            <div className="products-container">
-
-            </div>
-            <div className="cart-container">
-                <Cart cart={cart}></Cart>
-            </div>
         </div>
     );
 };
